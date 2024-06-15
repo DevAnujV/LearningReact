19b4ed193e6c1ccfaffb6f9a23bdc939b259a76b
Anuj Verma <elseanuj@gmail.com>
2024-06-15 17:50:13 -0400

    Learned to use ng-content, content projection by making a shared markup file and wrapping it around other content

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,11 +1,12 @@
 import { Time } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { type Task } from './task.model'; // we can add keyword type there just to specify that it is type that we imported nothing like an extension or library!
+import { CardComponent } from '../../shared/card/card.component';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
