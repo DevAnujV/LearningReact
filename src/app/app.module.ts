b18b932f5dcfbaf147fcd586e68479007bfaea91
Anuj Verma <elseanuj@gmail.com>
2024-06-16 02:56:00 -0400

    Converted whole app to module based. Now we have one signle module file that declared all the Components that need to work together

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,24 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { TaskComponent } from './tasks/task/task.component';
+import { NgFor, NgIf } from '@angular/common';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { CardComponent } from './shared/card/card.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent], // we add our non stand alone components here, starting with the root component, in our case app-root
+  declarations: [
+    AppComponent,
+    TasksComponent,
+    HeaderComponent,
+    UserComponent,
+    TaskComponent,
+    CardComponent,
+    NewTaskComponent,
+  ], // we add our non stand alone components here, starting with the root component, in our case app-root
   bootstrap: [AppComponent], // this way we tell angular app that this is root component with which I want to start application
-  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent], // here we add our stand alone components that are dependent on App Component.
+  imports: [BrowserModule, NgFor, FormsModule, NgIf], // here we add our stand alone components that are dependent on App Component.
   // BrowserModule is required for app to work
 })
 export class AppModule {}
--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,14 +1,10 @@
-import { Time } from '@angular/common';
-import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { type Task } from './task.model'; // we can add keyword type there just to specify that it is type that we imported nothing like an extension or library!
-import { CardComponent } from '../../shared/card/card.component';
-import { DatePipe } from '@angular/common';
+
 import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-task',
-  standalone: true,
-  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
